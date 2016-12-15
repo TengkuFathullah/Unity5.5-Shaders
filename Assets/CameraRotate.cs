5d34a4387a2771623a18ef9f186107b648b4dd87
Tengku <tgfathullah.tgmanshor@contentcreation.com.my>
2016-12-16 01:07:26 +0800

    Add view script(rotate)

---
+++ Assets/CameraRotate.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotate : MonoBehaviour
+{
+	Camera cam;
+
+	void Awake()
+	{
+		cam = GetComponent<Camera>();
+	}
+
+	void Update()
+	{
+        transform.RotateAround(Vector3.zero, Vector3.up, 1f);
+	}
+}
