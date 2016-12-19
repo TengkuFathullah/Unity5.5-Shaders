d67fa09c064b781181762e9e0f75d65b6e7c3bc0
Tengku <tgfathullah.tgmanshor@contentcreation.com.my>
2016-12-19 21:29:20 +0800

    Add toggle panel ui

--- Assets/Scripts/ShaderLabUI.cs
+++ Assets/Scripts/ShaderLabUI.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class ShaderLabUI : MonoBehaviour 
 {
@@ -15,6 +16,20 @@ public class ShaderLabUI : MonoBehaviour
     public void Static()
     {
         camRotate.enabled = false;
+    }
+    #endregion
+
+    #region UI
+    public RectTransform ToolsPanel;
+    bool isEnable;
+    public void TogglePanel()
+    {
+        if (!isEnable)
+            ToolsPanel.DOAnchorPosX(-196, 0.5f);
+        else
+            ToolsPanel.DOAnchorPosX(0, 0.5f);
+
+        isEnable = !isEnable;
     }
     #endregion
 
