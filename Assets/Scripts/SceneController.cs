5f2a06651a52d4fad0266d166df221854cbba94d
Tengku <tgfathullah.tgmanshor@contentcreation.com.my>
2017-01-05 22:19:52 +0800

    Add scene controller script

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneController : MonoBehaviour
+{
+    public Shader[] shaders;
+    public GameObject[] models;
+
+    GameObject targetObject;
+
+    void Start()
+    {
+        targetObject = models[0];
+    }
+
+    int currentShaderIndex 
+    {
+        get
+        {
+            return PlayerPrefs.GetInt("shader_index", 0);
+        }
+        set
+        {
+            PlayerPrefs.SetInt("shader_index", value);
+        }
+    }
+
+    public void SwitchShader()
+    {
+        currentShaderIndex++;
+
+        Renderer[] renders = targetObject.GetComponentsInChildren<Renderer>();
+
+        foreach(Renderer render in renders)
+        {
+            render.material.shader = shaders[currentShaderIndex];
+        }
+    }
+	
+}
